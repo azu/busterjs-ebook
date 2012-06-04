4f5e0068e4af41aab7d11e5c0dd02dcac28294c6
azu <azuciao@gmail.com>
2012-06-04 22:36:26 +0900

    apply sphinx-theme-okfn theme

--- conf.py
+++ conf.py
@@ -94,8 +94,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'scrolls'
-
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
+html_theme = 'sphinx-theme-okfn'
+html_theme_options = {
+        'logo_icon': 'http://busterjs.readthedocs.org/en/latest/_static/buster-logo.png'
+}
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
