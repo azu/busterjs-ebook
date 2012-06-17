130730f7bbd4284137d2759c981f1d01b40f1649
azu <azuciao@gmail.com>
2012-06-17 16:38:08 +0900

    theme change

--- conf.py
+++ conf.py
@@ -100,6 +100,8 @@ sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
 html_theme = 'sphinx-theme-okfn'
 html_theme_options = {
+		'show_okfn_logo': False,
+		'git_repo': 'https://github.com/azu/busterjs-ebook',
         'logo_icon': 'http://busterjs.readthedocs.org/en/latest/_static/buster-logo.png'
 }
 # Theme options are theme-specific and customize the look and feel of a theme
